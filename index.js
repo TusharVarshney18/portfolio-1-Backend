4cfdb0eb5e218dad07b7ead74d88ee3325de4710
Tushar <tusharvarshney1810@gmail.com>
2025-07-27 12:04:00 +0530

    added cors origin

--- index.js
+++ index.js
@@ -20,3 +20,7 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
    console.log(`Server running on port ${port}`);
 });
+app.use((req, res, next) => {
+   res.header('Access-Control-Allow-Origin', 'https://portfolio-1-ten-mocha.vercel.app');
+   next();
+});
