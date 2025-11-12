60dad6baee26ba1eb6f263c1a2479b141d6ffa2e
TUSHAR VARSHNEY <tusharvarshney1810@gmail.com>
2025-11-12 23:11:01 +0530

    added frontend url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express';
-import cors from 'cors';
-import dotenv from 'dotenv';
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 dotenv.config();
 
@@ -9,18 +9,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-   res.send('Hello from Vercel!');
+app.get("/", (req, res) => {
+  res.send("Hello from Vercel!");
 });
 
-import sendEmailRouter from './api/send-email.js';
-app.use('/api', sendEmailRouter);
+import sendEmailRouter from "./api/send-email.js";
+app.use("/api", sendEmailRouter);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-   console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
 app.use((req, res, next) => {
-   res.header('Access-Control-Allow-Origin', 'https://portfolio-1-ten-mocha.vercel.app');
-   next();
+  res.header("Access-Control-Allow-Origin", "https://portfolio-1-ten-mocha.vercel.app", "https://portfolio-2-frontend-umber.vercel.app");
+  next();
 });
